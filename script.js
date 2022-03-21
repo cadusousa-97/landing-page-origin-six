a399b905eda46aa01f5dc78b3d8e50645b03e144
cadusousa <cadusb2@gmail.com>
2022-03-21 20:20:42 -0300

    Sessões: depoimentos e contatos adicionadas. Inclusão da biblioteca scroll reveal para melhorar o visual da página

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ for (const link of links) {
   })
 }
 
-//MUDAR O HEADER DA PAGINA QUANDO DER SCROLL
+/*MUDAR O HEADER DA PAGINA QUANDO DER SCROLL*/
 
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
@@ -26,3 +26,34 @@ window.addEventListener('scroll', () => {
     header.classList.remove('scroll')
   }
 })
+
+/*TESTIMONIALS CAROUSEL SLIDER SWIPER */
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* SCROLL REVEAL: Mostrar elementos quando der scroll na página */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home .text, #home .image,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+`,
+  { interval: 100 }
+)
